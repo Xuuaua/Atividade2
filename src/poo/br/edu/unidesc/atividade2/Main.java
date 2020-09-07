e6f7b0c31c458bfc31fbf7ec707935f617a9c567
Xuuaua <vitor.v1999@gmail.com>
2020-09-07 15:52:47 -0300

    Inicio

---
+++ src/poo/br/edu/unidesc/atividade2/Main.java
@@ -0,0 +1,24 @@
+package poo.br.edu.unidesc.atividade2;
+
+//Atividade: Trabalho de Revisão para VA
+// Professor: Wesley Queiroz
+// Materia: Programação orientada a objetos
+// Aluno: Vitor Hugo Da Costa Sousa
+
+/*  Com base nos seus conhecimentos de Herança crie as classes necessárias, conforme:
+    -Crie uma classe chamada Animal(com as propriedades raça e idade)
+    -Implemente um método emitirSom(). Esse método imprime uma saída no console "Animal Emitindo som"
+    -Crie 3 subclasses, que herdam de Animal:
+        -Cao (crie propriedades específicas para um cachorro. Use sua imaginação)
+        -Gato (crie propriedades específicas para um gato. Use sua imaginação)
+    -Sobrescreva o método emitirSom() para as subclasses alterando o comportamento para cada subclasse
+    -Crie uma classe e instâncie um cão e um gato e execute os métodos emitirSom de cada um deles.
+
+        */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
